e76109e00e598571e60ebfacb9ad193bf0de6790
ezradiniz <linux.esdras@gmail.com>
2019-07-28 23:25:51 -0300

    Fix utils/docker-compose

--- utils/docker-compose.sh
+++ utils/docker-compose.sh
@@ -1,4 +1,5 @@
-udo apt install curl
+#!/bin/sh
+sudo apt install curl
 sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 su - $USER
