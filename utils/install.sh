0d0ed5f4eaa199098bea7cf2ee49f63f16ec345c
Mateus Esdras <linux.esdras@gmail.com>
2021-07-10 21:30:39 -0300

    fix(utils): prevent globbing and word splitting

--- utils/install.sh
+++ utils/install.sh
@@ -19,7 +19,7 @@ yay -S ttf-nerd-fonts-hack-complete-git
 
 # terminal
 yay -S tmux zsh oh-my-zsh-git
-chsh -s $(which zsh)
+chsh -s "$(which zsh)"
 
 # tools
 yay -S fzf fzf-extras ripgrep fd bat ranger
@@ -27,7 +27,7 @@ yay -S fzf fzf-extras ripgrep fd bat ranger
 # docker
 yay -S docker
 sudo systemctl enable docker
-sudo usermod -aG docker $USER
+sudo usermod -aG docker "$USER"
 
 curl -L https://github.com/docker/compose/releases/download/1.27.4/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
