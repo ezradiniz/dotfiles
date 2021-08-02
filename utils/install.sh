a129f29a7b2dd22000138c2b56eb6428f40cd852
Mateus Esdras <linux.esdras@gmail.com>
2021-08-01 22:39:42 -0300

    fix(utils): update docker-compose version

--- utils/install.sh
+++ utils/install.sh
@@ -29,5 +29,5 @@ yay -S docker
 sudo systemctl enable docker
 sudo usermod -aG docker "$USER"
 
-curl -L https://github.com/docker/compose/releases/download/1.27.4/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.29.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
