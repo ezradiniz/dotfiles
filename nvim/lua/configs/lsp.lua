f27c91ecd70370d00c63d9e634dd3b36a40822a5
Mateus Esdras <linux.esdras@gmail.com>
2021-08-31 20:17:53 -0300

    feat(nvim): add lua language server

--- nvim/lua/configs/lsp.lua
+++ nvim/lua/configs/lsp.lua
@@ -61,3 +61,30 @@ for _, lsp in ipairs(servers) do
         flags = {debounce_text_changes = 150}
     }
 end
+
+nvim_lsp.sumneko_lua.setup {
+    cmd = {'lua-language-server'},
+    on_attach = on_attach,
+    settings = {
+        Lua = {
+            runtime = {
+                -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+                version = 'LuaJIT',
+                -- Setup your lua path
+                path = vim.split(package.path, ';')
+            },
+            diagnostics = {
+                -- Get the language server to recognize the `vim` global
+                globals = {'vim', "use"},
+                disable = {"lowercase-global"}
+            },
+            workspace = {
+                -- Make the server aware of Neovim runtime files
+                library = {
+                    [vim.fn.expand('$VIMRUNTIME/lua')] = true,
+                    [vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true
+                }
+            }
+        }
+    }
+}
