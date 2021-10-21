1efbc1c6f69bd74ffe04d23c60dede4e5fec3c4f
Mateus Esdras <linux.esdras@gmail.com>
2021-10-21 07:53:08 -0300

    refactor(nvim): update fugitive mappings

--- nvim/lua/configs/fugitive.lua
+++ nvim/lua/configs/fugitive.lua
@@ -5,20 +5,17 @@ vim.g.fugitive_pty = false
 vim.api.nvim_set_keymap('n', '<Leader>G', ':Git ', {noremap = true})
 vim.api.nvim_set_keymap('n', '<Leader>gs', ':Git<cr>', opts)
 vim.api.nvim_set_keymap('n', '<Leader>gps', ':Gpush<cr>', opts)
-vim.api.nvim_set_keymap('n', '<Leader>gpf', ':Gpush --force-with-lease<cr>',
-                        opts)
+vim.api.nvim_set_keymap('n', '<Leader>gpf', ':Gpush --force-with-lease<cr>', opts)
 vim.api.nvim_set_keymap('n', '<Leader>gd', ':Gdiff<cr>', opts)
 vim.api.nvim_set_keymap('n', '<Leader>gf', ':Gfetch<cr>', opts)
-vim.api.nvim_set_keymap('n', '<Leader>gm', ':Git merge<cr>', opts)
+vim.api.nvim_set_keymap('n', '<Leader>gmt', ':Git mergetool<cr>', opts)
 vim.api.nvim_set_keymap('n', '<Leader>gbr', ':Git branch<cr>', opts)
 vim.api.nvim_set_keymap('n', '<Leader>gbl', ':Git blame<cr>', opts)
 vim.api.nvim_set_keymap('n', '<Leader>gh', ':Git show<cr>', opts)
-vim.api.nvim_set_keymap('n', '<Leader>gl',
-                        ':Git log --oneline --decorate --graph<cr>', opts)
-vim.api.nvim_set_keymap('n', '<Leader>0gl',
-                        ':Git log --oneline --decorate --graph %<cr>', opts)
+vim.api.nvim_set_keymap('n', '<Leader>gvd', ':Gvdiffsplit!<cr>', opts)
+vim.api.nvim_set_keymap('n', '<Leader>gl', ':Git log --oneline --decorate --graph<cr>', opts)
+vim.api.nvim_set_keymap('n', '<Leader>0gl', ':Git log --oneline --decorate --graph %<cr>', opts)
 
--- TODO: refactor to lua
 vim.cmd(
     [[ command! -bang -bar -nargs=* Gpush execute 'Dispatch<bang> -dir=' . fnameescape(FugitiveGitDir()) 'git push' <q-args> ]])
 vim.cmd(
