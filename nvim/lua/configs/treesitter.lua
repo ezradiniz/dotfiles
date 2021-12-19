a2362ef2cfb82e8ec50a0c29deac264026a566ba
Mateus Esdras <linux.esdras@gmail.com>
2021-12-18 23:46:43 -0300

    nvim: update treesitter highlight

--- nvim/lua/configs/treesitter.lua
+++ nvim/lua/configs/treesitter.lua
@@ -1,5 +1,8 @@
 require'nvim-treesitter.configs'.setup {
-    highlight = {enable = true},
+    highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = true,
+    },
     incremental_selection = {enable = true},
     textobjects = {enable = true}
 }
