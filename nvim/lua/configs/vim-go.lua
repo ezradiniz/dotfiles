9833b991f4366514fb0b88078cfc10a85641ab67
Mateus Esdras <linux.esdras@gmail.com>
2022-02-05 23:22:33 -0300

    nvim: update vim-go

--- nvim/lua/configs/vim-go.lua
+++ nvim/lua/configs/vim-go.lua
@@ -1,6 +1,5 @@
-vim.g.go_gopls_enabled = 1
 vim.g.go_def_mapping_enabled = 0
 vim.g.go_code_completion_enabled = 0
 vim.g.go_list_type = "quickfix"
-vim.g.go_fmt_command = "goimports"
 vim.g.go_fmt_fail_silently = 1
+vim.g.go_diagnostics_enabled = 0
