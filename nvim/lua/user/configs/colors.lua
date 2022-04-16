4aa891adc6d7decd6113a480807d8e526225434f
Mateus Esdras <linux.esdras@gmail.com>
2022-04-15 23:19:04 -0300

    nvim: lua format

--- nvim/lua/user/configs/colors.lua
+++ nvim/lua/user/configs/colors.lua
@@ -1,10 +1,10 @@
 vim.cmd("syntax enable")
 vim.opt.termguicolors = true
-vim.opt.background = 'dark'
+vim.opt.background = "dark"
 vim.cmd("colorscheme base16-default-dark")
 
 -- Diff highlights
-vim.cmd[[
+vim.cmd([[
     hi DiffAdd guifg=#a1b56c guibg=#181818 gui=inverse
     hi DiffChange guifg=#689d6a guibg=#181818 gui=inverse
     hi DiffDelete guifg=#ab4642 guibg=#181818 gui=inverse
@@ -15,4 +15,4 @@ vim.cmd[[
     hi diffChanged guifg=#689d6a
     hi diffNewFile guifg=#fabd2f
     hi diffLine guifg=#458588
-]]
+]])
