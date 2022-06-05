585cdcc6b73d90969b2d3fba8823d8496e64d8e1
Mateus Esdras <linux.esdras@gmail.com>
2022-06-04 23:36:43 -0300

    nvim: format

--- nvim/lua/user/configs/null-ls.lua
+++ nvim/lua/user/configs/null-ls.lua
@@ -4,13 +4,14 @@ local sources = {
     null_ls.builtins.formatting.prettier,
     null_ls.builtins.diagnostics.write_good,
     null_ls.builtins.code_actions.gitsigns,
-    null_ls.builtins.formatting.eslint_d, null_ls.builtins.diagnostics.eslint_d,
-    null_ls.builtins.formatting.stylua
+    null_ls.builtins.formatting.eslint_d,
+    null_ls.builtins.diagnostics.eslint_d,
+    null_ls.builtins.formatting.stylua,
 }
 
 null_ls.setup({
     sources = sources,
     should_attach = function(bufnr)
         return not vim.api.nvim_buf_get_name(bufnr):match("^git://")
-    end
+    end,
 })
