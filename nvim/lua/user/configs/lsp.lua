87989dc09420fde105e1406023b7e07b8d76b2f9
Mateus Esdras <linux.esdras@gmail.com>
2022-07-05 23:30:25 -0300

    nvim: refacotr lua lsp

--- nvim/lua/user/configs/lsp.lua
+++ nvim/lua/user/configs/lsp.lua
@@ -15,6 +15,7 @@ local on_attach = function(_, bufnr)
     local function buf_set_keymap(...)
         vim.api.nvim_buf_set_keymap(bufnr, ...)
     end
+
     local function buf_set_option(...)
         vim.api.nvim_buf_set_option(bufnr, ...)
     end
@@ -71,19 +72,14 @@ nvim_lsp.sumneko_lua.setup({
     on_attach = on_attach,
     settings = {
         Lua = {
-            runtime = {
-                -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-                version = "LuaJIT",
-                -- Setup your lua path
-                path = vim.split(package.path, ";"),
-            },
-            diagnostics = {
-                -- Get the language server to recognize the `vim` global
-                globals = { "vim", "use" },
-            },
+            runtime = { version = "LuaJIT", path = vim.split(package.path, ";") },
+            diagnostics = { globals = { "vim", "use" } },
             workspace = {
                 -- Make the server aware of Neovim runtime files
-                library = vim.api.nvim_get_runtime_file("", true),
+                library = {
+                    [vim.fn.expand("$VIMRUNTIME/lua")] = true,
+                    [vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true,
+                },
             },
         },
     },
