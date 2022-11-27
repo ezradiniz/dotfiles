f9840487941b45737f5dea89dbb17e55f75940b9
Mateus Esdras <linux.esdras@gmail.com>
2022-11-27 19:23:34 -0300

    nvim: disable slow treesitter highlight for large files

--- nvim/lua/user/configs/treesitter.lua
+++ nvim/lua/user/configs/treesitter.lua
@@ -19,7 +19,17 @@ require("nvim-treesitter.configs").setup({
         "lua",
     },
     auto_install = true,
-    highlight = { enable = true, additional_vim_regex_highlighting = true },
+    highlight = {
+        enable = true,
+        disable = function(_, buf)
+            local max_filesize = 100 * 1024 -- 100 KB
+            local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+            if ok and stats and stats.size > max_filesize then
+                return true
+            end
+        end,
+        additional_vim_regex_highlighting = false,
+    },
     autopairs = { enable = true },
     incremental_selection = { enable = true },
     textobjects = { enable = true },
