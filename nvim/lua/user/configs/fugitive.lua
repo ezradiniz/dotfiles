d017a82017fc11fafff1801deaa5656c9c5cdfbe
Mateus Esdras <linux.esdras@gmail.com>
2022-11-27 17:35:08 -0300

    nvim: remove fugitive deprecations

--- nvim/lua/user/configs/fugitive.lua
+++ nvim/lua/user/configs/fugitive.lua
@@ -1,12 +1,11 @@
 local opts = { noremap = true, silent = true }
 
-
 vim.keymap.set("n", "<Leader>G", ":Git ", { noremap = true })
 vim.keymap.set("n", "<Leader>gs", ":Git<cr>", opts)
-vim.keymap.set("n", "<Leader>gps", ":Gpush", { noremap = true })
-vim.keymap.set("n", "<Leader>gpf", ":Gpush --force-with-lease", { noremap = true })
-vim.keymap.set("n", "<Leader>gd", ":Gdiff<cr>", opts)
-vim.keymap.set("n", "<Leader>gf", ":Gfetch<cr>", opts)
+vim.keymap.set("n", "<Leader>gps", ":Git! push", opts)
+vim.keymap.set("n", "<Leader>gpf", ":Git! push --force-with-lease", opts)
+vim.keymap.set("n", "<Leader>gd", ":Gdiffsplit<cr>", opts)
+vim.keymap.set("n", "<Leader>gf", ":Git! fetch<cr>", opts)
 vim.keymap.set("n", "<Leader>gmt", ":Git mergetool<cr>", opts)
 vim.keymap.set("n", "<Leader>gbr", ":Git branch<cr>", opts)
 vim.keymap.set("n", "<Leader>gbl", ":Git blame<cr>", opts)
@@ -15,10 +14,3 @@ vim.keymap.set("n", "<Leader>gvd", ":Gvdiffsplit!<cr>", opts)
 vim.keymap.set("n", "<Leader>gl", ":Git log --oneline --decorate --graph<cr>", opts)
 vim.keymap.set("n", "<Leader>0gl", ":Git log --oneline --decorate --graph %<cr>", opts)
 vim.keymap.set("n", "<Leader>0gcl", ":0Gclog<cr>", opts)
-
-vim.cmd(
-    [[ command! -bang -bar -nargs=* Gpush execute 'Dispatch<bang> -dir=' . fnameescape(FugitiveGitDir()) 'git push' <q-args> ]]
-)
-vim.cmd(
-    [[ command! -bang -bar -nargs=* Gfetch execute 'Dispatch<bang> -dir=' . fnameescape(FugitiveGitDir()) 'git fetch --prune' <q-args> ]]
-)
