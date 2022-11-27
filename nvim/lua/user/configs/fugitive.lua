b24fdefa16335b986cfc0ccd8f92769165bed412
Mateus Esdras <linux.esdras@gmail.com>
2022-11-27 17:21:14 -0300

    nvim: change fugitive to use vim.keymap.set

--- nvim/lua/user/configs/fugitive.lua
+++ nvim/lua/user/configs/fugitive.lua
@@ -2,20 +2,20 @@ local opts = { noremap = true, silent = true }
 
 vim.g.fugitive_pty = false
 
-vim.api.nvim_set_keymap("n", "<Leader>G", ":Git ", { noremap = true })
-vim.api.nvim_set_keymap("n", "<Leader>gs", ":Git<cr>", opts)
-vim.api.nvim_set_keymap("n", "<Leader>gps", ":Gpush", { noremap = true })
-vim.api.nvim_set_keymap("n", "<Leader>gpf", ":Gpush --force-with-lease", { noremap = true })
-vim.api.nvim_set_keymap("n", "<Leader>gd", ":Gdiff<cr>", opts)
-vim.api.nvim_set_keymap("n", "<Leader>gf", ":Gfetch<cr>", opts)
-vim.api.nvim_set_keymap("n", "<Leader>gmt", ":Git mergetool<cr>", opts)
-vim.api.nvim_set_keymap("n", "<Leader>gbr", ":Git branch<cr>", opts)
-vim.api.nvim_set_keymap("n", "<Leader>gbl", ":Git blame<cr>", opts)
-vim.api.nvim_set_keymap("n", "<Leader>gh", ":Git show<cr>", opts)
-vim.api.nvim_set_keymap("n", "<Leader>gvd", ":Gvdiffsplit!<cr>", opts)
-vim.api.nvim_set_keymap("n", "<Leader>gl", ":Git log --oneline --decorate --graph<cr>", opts)
-vim.api.nvim_set_keymap("n", "<Leader>0gl", ":Git log --oneline --decorate --graph %<cr>", opts)
-vim.api.nvim_set_keymap("n", "<Leader>0gcl", ":0Gclog<cr>", opts)
+vim.keymap.set("n", "<Leader>G", ":Git ", { noremap = true })
+vim.keymap.set("n", "<Leader>gs", ":Git<cr>", opts)
+vim.keymap.set("n", "<Leader>gps", ":Gpush", { noremap = true })
+vim.keymap.set("n", "<Leader>gpf", ":Gpush --force-with-lease", { noremap = true })
+vim.keymap.set("n", "<Leader>gd", ":Gdiff<cr>", opts)
+vim.keymap.set("n", "<Leader>gf", ":Gfetch<cr>", opts)
+vim.keymap.set("n", "<Leader>gmt", ":Git mergetool<cr>", opts)
+vim.keymap.set("n", "<Leader>gbr", ":Git branch<cr>", opts)
+vim.keymap.set("n", "<Leader>gbl", ":Git blame<cr>", opts)
+vim.keymap.set("n", "<Leader>gh", ":Git show<cr>", opts)
+vim.keymap.set("n", "<Leader>gvd", ":Gvdiffsplit!<cr>", opts)
+vim.keymap.set("n", "<Leader>gl", ":Git log --oneline --decorate --graph<cr>", opts)
+vim.keymap.set("n", "<Leader>0gl", ":Git log --oneline --decorate --graph %<cr>", opts)
+vim.keymap.set("n", "<Leader>0gcl", ":0Gclog<cr>", opts)
 
 vim.cmd(
     [[ command! -bang -bar -nargs=* Gpush execute 'Dispatch<bang> -dir=' . fnameescape(FugitiveGitDir()) 'git push' <q-args> ]]
