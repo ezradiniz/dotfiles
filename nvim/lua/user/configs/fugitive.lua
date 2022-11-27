0113dfa69e9b8f73075f1fd843834dce68cf0a1a
Mateus Esdras <linux.esdras@gmail.com>
2022-11-27 18:18:19 -0300

    nvim: fix keymap options

--- nvim/lua/user/configs/fugitive.lua
+++ nvim/lua/user/configs/fugitive.lua
@@ -2,8 +2,8 @@ local opts = { noremap = true, silent = true }
 
 vim.keymap.set("n", "<Leader>G", ":Git ", { noremap = true })
 vim.keymap.set("n", "<Leader>gs", ":Git<cr>", opts)
-vim.keymap.set("n", "<Leader>gps", ":Git! push", opts)
-vim.keymap.set("n", "<Leader>gpf", ":Git! push --force-with-lease", opts)
+vim.keymap.set("n", "<Leader>gps", ":Git! push", { noremap = true })
+vim.keymap.set("n", "<Leader>gpf", ":Git! push --force-with-lease", { noremap = true })
 vim.keymap.set("n", "<Leader>gd", ":Gdiffsplit<cr>", opts)
 vim.keymap.set("n", "<Leader>gf", ":Git! fetch<cr>", opts)
 vim.keymap.set("n", "<Leader>gmt", ":Git mergetool<cr>", opts)
