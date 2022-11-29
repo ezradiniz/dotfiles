f54a12358afd9563863e97b480e2b538dfdbc5c3
Mateus Esdras <linux.esdras@gmail.com>
2022-11-28 22:27:15 -0300

    nvim: remove null_ls write_good

--- nvim/lua/user/configs/null-ls.lua
+++ nvim/lua/user/configs/null-ls.lua
@@ -2,7 +2,6 @@ local null_ls = require("null-ls")
 
 local sources = {
     null_ls.builtins.formatting.prettier,
-    null_ls.builtins.diagnostics.write_good,
     null_ls.builtins.code_actions.gitsigns,
     null_ls.builtins.formatting.eslint_d,
     null_ls.builtins.diagnostics.eslint_d,
