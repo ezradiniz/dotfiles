6f23d5e9d7554f26627a6d5d628664948cb501ac
Mateus Esdras <linux.esdras@gmail.com>
2022-12-24 13:00:41 -0300

    nvim: use find_files as fallback for git_files

--- nvim/lua/user/configs/telescope.lua
+++ nvim/lua/user/configs/telescope.lua
@@ -5,17 +5,24 @@ local builtin = require("telescope.builtin")
 
 local opts = { noremap = true, silent = true }
 
+local function find_files()
+    builtin.find_files({ find_command = { "rg", "--files", "--hidden", "--smart-case", "--glob=!.git" } })
+end
+
 vim.keymap.set("n", "<Leader>fb", builtin.buffers, opts)
 vim.keymap.set("n", "<Leader>fg", builtin.live_grep, opts)
-vim.keymap.set("n", "<Leader>fp", builtin.git_files, opts)
 vim.keymap.set("n", "<space>Q", builtin.diagnostics, opts)
 vim.keymap.set("n", "<Leader>fr", builtin.resume, opts)
 vim.keymap.set("n", "<Leader>fh", builtin.help_tags, opts)
 vim.keymap.set("n", "<Leader>/", builtin.current_buffer_fuzzy_find, opts)
 vim.keymap.set("n", "q:", builtin.command_history, opts)
 vim.keymap.set("n", "<Leader>fm", builtin.marks, opts)
-vim.keymap.set("n", "<Leader>fl", function()
-    builtin.find_files({ find_command = { "rg", "--files", "--hidden", "--smart-case", "--glob=!.git" } })
+vim.keymap.set("n", "<Leader>fl", find_files, opts)
+vim.keymap.set("n", "<Leader>fp", function()
+    local ok, _ = pcall(builtin.git_files)
+    if not ok then
+        find_files()
+    end
 end, opts)
 
 telescope.setup({
