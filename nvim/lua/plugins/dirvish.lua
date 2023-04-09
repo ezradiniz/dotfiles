a2489e85ff822b5f531c1cef662dc156fe3097eb
Mateus Esdras <linux.esdras@gmail.com>
2023-04-09 14:06:11 -0300

    nvim: remove dirvish unmap

--- nvim/lua/plugins/dirvish.lua
+++ nvim/lua/plugins/dirvish.lua
@@ -1,11 +1,3 @@
 return {
   "justinmk/vim-dirvish",
-  config = function()
-    vim.cmd([[
-      augroup dirvish_config
-          autocmd!
-          autocmd FileType dirvish silent! unmap <buffer> <C-p>
-      augroup end
-    ]])
-  end,
 }
