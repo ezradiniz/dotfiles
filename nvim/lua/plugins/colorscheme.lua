97c2dbc8df0390a4efa4466439329066320470cc
Mateus Esdras <linux.esdras@gmail.com>
2023-06-15 22:32:15 -0300

    nvim: use gruvbox

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,40 +1,11 @@
 return {
-  "catppuccin/nvim",
-  name = "catppuccin",
+  "ellisonleao/gruvbox.nvim",
   lazy = false,
   priority = 1000,
   config = function()
-    require("catppuccin").setup({
-      flavour = "mocha",
-      term_colors = true,
-      transparent_background = false,
-      no_italic = false,
-      no_bold = false,
-      color_overrides = {
-        mocha = {
-          base = "#181818",
-          -- NOTE(temporarily): This fixes background colors for other plugins
-          mantle = "#181818",
-        },
-      },
-      highlight_overrides = {
-        all = function(_)
-          return {
-            Search = { fg = "#181818", bg = "#f9e2af" },
-            IncSearch = { bg = "#f1fa6a" },
-          }
-        end,
-      },
-      integrations = {
-        cmp = true,
-        treesitter = true,
-        gitsigns = true,
-        telescope = true,
-        native_lsp = {
-          enabled = true,
-        },
-      },
+    require("gruvbox").setup({
+      contrast = "hard",
     })
-    vim.cmd.colorscheme("catppuccin")
+    vim.cmd.colorscheme("gruvbox")
   end,
 }
