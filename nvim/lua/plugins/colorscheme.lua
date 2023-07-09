6eeb286493fd832b4ffbe0d8fd57476da3608ee5
Mateus Esdras <linux.esdras@gmail.com>
2023-07-09 18:09:33 -0300

    nvim: adjust diff overrides

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -6,13 +6,14 @@ return {
     require("gruvbox").setup({
       contrast = "hard",
       overrides = {
-        DiffAdd  = { fg = "#a1b56c", bg = "#181818", reverse =  true },
-        DiffChange  = { fg = "#689d6a", bg = "#181818", reverse =  true },
-        DiffDelete  = { fg = "#ab4642", bg = "#181818", reverse =  true },
-        diffAdded = { fg = "#a1b56c", bg = "#181818", reverse =  true },
-        diffChanged = { fg = "#689d6a", bg = "#181818", reverse =  true },
-        diffRemoved = { fg = "#ab4642", bg = "#181818", reverse =  true },
-      }
+        DiffAdd = { fg = "#a1b56c", bg = "#181818", reverse = true },
+        DiffChange = { fg = "#689d6a", bg = "#181818", reverse = true },
+        DiffDelete = { fg = "#ab4642", bg = "#181818", reverse = true },
+        DiffText = { fg = "#458588", bg = "#181818", reverse = true },
+        diffAdded = { fg = "#a1b56c", bg = "#181818", reverse = true },
+        diffChanged = { fg = "#689d6a", bg = "#181818", reverse = true },
+        diffRemoved = { fg = "#ab4642", bg = "#181818", reverse = true },
+      },
     })
     vim.opt.background = "light"
     vim.cmd.colorscheme("gruvbox")
