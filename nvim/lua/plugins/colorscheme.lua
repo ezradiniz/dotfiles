747c73b29759a350bba60069cd16d74e6a601498
Mateus Esdras <linux.esdras@gmail.com>
2023-07-09 18:02:07 -0300

    nvim: add light theme

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -5,7 +5,16 @@ return {
   config = function()
     require("gruvbox").setup({
       contrast = "hard",
+      overrides = {
+        DiffAdd  = { fg = "#a1b56c", bg = "#181818", reverse =  true },
+        DiffChange  = { fg = "#689d6a", bg = "#181818", reverse =  true },
+        DiffDelete  = { fg = "#ab4642", bg = "#181818", reverse =  true },
+        diffAdded = { fg = "#a1b56c", bg = "#181818", reverse =  true },
+        diffChanged = { fg = "#689d6a", bg = "#181818", reverse =  true },
+        diffRemoved = { fg = "#ab4642", bg = "#181818", reverse =  true },
+      }
     })
+    vim.opt.background = "light"
     vim.cmd.colorscheme("gruvbox")
   end,
 }
