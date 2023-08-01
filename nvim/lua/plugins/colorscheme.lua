f70674221f56b4ce734d4335e07fdd4129be7628
Mateus Esdras <linux.esdras@gmail.com>
2023-07-31 21:13:19 -0300

    nvim: fix contrast

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -4,7 +4,7 @@ return {
   priority = 1000,
   config = function()
     require("gruvbox").setup({
-      contrast = "hard",
+      contrast = "soft",
       overrides = {
         DiffAdd = { fg = "#a1b56c", bg = "#181818", reverse = true },
         DiffChange = { fg = "#689d6a", bg = "#181818", reverse = true },
@@ -13,7 +13,9 @@ return {
         diffAdded = { fg = "#a1b56c", bg = "#181818", reverse = true },
         diffChanged = { fg = "#689d6a", bg = "#181818", reverse = true },
         diffRemoved = { fg = "#ab4642", bg = "#181818", reverse = true },
-      },
+        DiagnosticVirtualTextWarn  = { fg = "#7c6f64" },
+        DiagnosticVirtualTextError = { fg = "#ab4642" },
+     },
     })
     vim.opt.background = "light"
     vim.cmd.colorscheme("gruvbox")
