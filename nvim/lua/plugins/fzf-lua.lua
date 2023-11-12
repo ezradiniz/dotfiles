9b0e5fd77f64c0c6f232245ffa112f4c78a9b213
Mateus Esdras <linux.esdras@gmail.com>
2023-11-11 23:06:12 -0300

    nvim: unset resume=true to fzf-lua

--- nvim/lua/plugins/fzf-lua.lua
+++ nvim/lua/plugins/fzf-lua.lua
@@ -7,7 +7,7 @@ return {
     { "<leader>Q", "<cmd>FzfLua lsp_document_diagnostics<cr>" },
     { "<leader>fg", "<cmd>FzfLua live_grep_glob<cr>" },
     { "<leader>fg", "<cmd>FzfLua grep_visual<cr>", mode = "x" },
-    { "<leader>fp", "<cmd>FzfLua files resume=true<cr>" },
+    { "<leader>fp", "<cmd>FzfLua files<cr>" },
     { "<leader>fb", "<cmd>FzfLua buffers<cr>" },
     { "<leader>fo", "<cmd>FzfLua oldfiles<cr>" },
     { "<leader>fr", "<cmd>FzfLua resume<cr>" },
