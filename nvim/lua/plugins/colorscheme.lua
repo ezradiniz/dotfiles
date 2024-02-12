2734eadb7549752b36016516eee16eea551012b0
Mateus Esdras <linux.esdras@gmail.com>
2024-02-12 14:52:58 -0300

    nvim: use base16-default-dark colorscheme

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,43 +1,17 @@
 return {
-  "ellisonleao/gruvbox.nvim",
+  "RRethy/base16-nvim",
   lazy = false,
   priority = 1000,
   config = function()
-    -- TODO: Create new theme based on gruvbox and acme
-    require("gruvbox").setup({
-      contrast = "soft",
-      palette_overrides = {
-        light0_soft = "#ffffd7",
-      },
-      overrides = {
-        DiffAdd = { fg = "#a1b56c", bg = "#181818", reverse = true },
-        DiffChange = { fg = "#689d6a", bg = "#181818", reverse = true },
-        DiffDelete = { fg = "#ab4642", bg = "#181818", reverse = true },
-        DiffText = { fg = "#458588", bg = "#181818", reverse = true },
-        diffAdded = { fg = "#a1b56c", bg = "#181818", reverse = true },
-        diffChanged = { fg = "#689d6a", bg = "#181818", reverse = true },
-        diffRemoved = { fg = "#ab4642", bg = "#181818", reverse = true },
-        DiagnosticVirtualTextWarn = { fg = "#7c6f64" },
-        DiagnosticVirtualTextError = { fg = "#ab4642" },
-
-        Normal = { fg = "#000000", bg = "#ffffea" },
-        Constant = { fg = "", bg = "" },
-        Statement = { fg = "", bg = "" },
-        Identifier = { fg = "", bg = "" },
-        Ignore = { fg = "", bg = "" },
-        Delimiter = { fg = "", bg = "" },
-        String = { fg = "#005500", bg = "" },
-        Visual = { bg = "#ffffaf" },
-        Type = { fg = "#004488", bg = "" },
-        StatusLine = { fg = "", bg = "#eaeaea" },
-        StatusLineNC = { fg = "", bg = "#F9F5D7" },
-        CursorLineNr = { fg = "", bg = "", bold = true },
-        CursorLine = { fg = "", bg = "" },
-        ColorColumn = { fg = "", bg = "#F9F5D7" },
-        SignColumn = { fg = "", bg = "#F9F5D7" },
-      },
-    })
-    vim.opt.background = "light"
-    vim.cmd.colorscheme("gruvbox")
+    vim.opt.background = "dark"
+    vim.cmd.colorscheme("base16-default-dark")
+    vim.api.nvim_set_hl(0, "DiffAdd", { fg = "#a1b56c", bg = "#181818", reverse = true })
+    vim.api.nvim_set_hl(0, "DiffChange", { fg = "#689d6a", bg = "#181818", reverse = true })
+    vim.api.nvim_set_hl(0, "DiffDelete", { fg = "#ab4642", bg = "#181818", reverse = true })
+    vim.api.nvim_set_hl(0, "DiffText", { fg = "#458588", bg = "#181818", reverse = true })
+    vim.api.nvim_set_hl(0, "diffAdded", { fg = "#a1b56c" })
+    vim.api.nvim_set_hl(0, "diffChanged", { fg = "#689d6a" })
+    vim.api.nvim_set_hl(0, "diffNewFile", { fg = "#fabd2f" })
+    vim.api.nvim_set_hl(0, "diffLine", { fg = "#458588" })
   end,
 }
