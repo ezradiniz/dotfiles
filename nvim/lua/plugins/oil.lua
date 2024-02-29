eb77205e1910f000ff62b30ca9ebf1839ecc3d04
Mateus Esdras <linux.esdras@gmail.com>
2024-02-28 22:12:24 -0300

    nvim: show permissions, size and mtime

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -2,6 +2,15 @@ return {
   "stevearc/oil.nvim",
   opts = {
     default_file_explorer = false,
+    columns = {
+      "permissions",
+      "size",
+      "mtime",
+    },
+    buf_options = {
+      buflisted = false,
+      bufhidden = "hide",
+    },
   },
   keys = function()
     local oil = require("oil")
