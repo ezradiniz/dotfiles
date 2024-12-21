364f5fdc7c1c88a6b257228c3effd8f63e282b46
Mateus Esdras <linux.esdras@gmail.com>
2024-12-20 23:03:00 -0300

    nvim: remove fold autocmd

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -41,17 +41,3 @@ vim.api.nvim_create_autocmd("BufReadPost", {
   end,
 })
 
-vim.api.nvim_create_autocmd("FileType", {
-  callback = function()
-    vim.opt.foldlevel = 99
-    if require("nvim-treesitter.parsers").has_parser() then
-      vim.opt.foldmethod = "expr"
-      vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
-      vim.wo.foldtext = "v:lua.vim.treesitter.foldtext()"
-      vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
-    else
-      vim.opt.foldmethod = "manual"
-    end
-  end,
-  group = vim.api.nvim_create_augroup("foldmethod", { clear = true }),
-})
