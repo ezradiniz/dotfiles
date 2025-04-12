ea429a533ac5df147e3a81f3cb8450e7c4962182
Mateus Esdras <linux.esdras@gmail.com>
2025-04-11 21:55:41 -0300

    nvim: improve blink: S-Tab, no auto_brackets, cmdline, ghost_text

--- nvim/lua/plugins/blink.lua
+++ nvim/lua/plugins/blink.lua
@@ -9,14 +9,21 @@ return {
       ["<C-\\>"] = { "hide", "fallback" },
       ["<C-n>"] = { "select_next", "show" },
       ["<Tab>"] = { "select_next", "snippet_forward", "fallback" },
+      ["<S-Tab>"] = { "select_prev" },
       ["<C-p>"] = { "select_prev" },
       ["<C-b>"] = { "scroll_documentation_up", "fallback" },
       ["<C-f>"] = { "scroll_documentation_down", "fallback" },
     },
-    completion = { documentation = { auto_show = true } },
+    signature = { enabled = true },
+    completion = {
+      accept = { auto_brackets = { enabled = false } },
+      documentation = { auto_show = true },
+      ghost_text = { enabled = true },
+    },
     sources = {
       default = { "lsp", "buffer", "path" },
     },
     fuzzy = { implementation = "prefer_rust_with_warning" },
+    cmdline = { enabled = true },
   },
 }
