7e348bdfee6e6e4acfc35a861c98d168d7d8ecda
Mateus Esdras <linux.esdras@gmail.com>
2025-08-23 10:44:49 -0300

    nvim: FzfLua live_grep_glob -> live_grep

--- nvim/lua/plugins/fzf-lua.lua
+++ nvim/lua/plugins/fzf-lua.lua
@@ -5,7 +5,7 @@ return {
     { "<leader>fm", "<cmd>FzfLua help_tags<cr>" },
     { "<leader>fm", "<cmd>FzfLua marks<cr>" },
     { "<leader>Q", "<cmd>FzfLua lsp_document_diagnostics<cr>" },
-    { "<leader>fg", "<cmd>FzfLua live_grep_glob<cr>" },
+    { "<leader>fg", "<cmd>FzfLua live_grep<cr>" },
     { "<leader>fg", "<cmd>FzfLua grep_visual<cr>", mode = "x" },
     { "<leader>fp", "<cmd>FzfLua files<cr>" },
     { "<leader>fb", "<cmd>FzfLua buffers<cr>" },
